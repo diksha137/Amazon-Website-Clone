1c5f3978dda6add9ca1dc9f4b173e768dab85f1d
diksha <diksha.id202@gmail.com>
2022-07-16 08:38:01 +0530

    code with latest version of react-router-dom(6.3)//Switch->Routes

--- src/App.js
+++ src/App.js
@@ -2,27 +2,17 @@ import React from 'react'
 import Header from './Header/Header.js'
 import Home from './Home/Home.js'
 import Checkout from './Checkout/Checkout.js'
-import { BrowserRouter as  Router,Switch,Route } from "react-router-dom"
+import { BrowserRouter as  Router,Routes,Route } from "react-router-dom"
 
 function App() {
   return (
   
   <Router>
       <div className='app'>
-        <Switch>
-
-          <Route path="/checkout">
-            <Header/>
-            <Checkout/>
-          </Route>
-
-          <Route path="/">
-            <Header/>
-            <Home/>
-          </Route>
-
-          
-        </Switch>
+        <Routes>
+             <Route  path="/" element= {<><Header/><Home/></>} />
+             <Route  path="/checkout" element= {<><Header/><Checkout/></>} />
+        </Routes>
       </div>
   </Router>
 
